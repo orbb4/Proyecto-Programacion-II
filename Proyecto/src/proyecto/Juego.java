147ecd21bd5a14ef353659cdf78f2ca1aa843b48
BAstian <bas_bece@hotmail.com>
2022-11-22 11:21:07 -0300

    Implementacion de control por teclas

--- Proyecto/src/proyecto/Juego.java
+++ Proyecto/src/proyecto/Juego.java
@@ -1,9 +1,14 @@
 package proyecto;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 public class Juego extends JPanel{
     public Juego(){
+        this.setFocusable(true);
         this.setLayout(null); 
+        this.addKeyListener(new KeyChecker());
         
     }
     public void paint(Graphics g){       
@@ -14,5 +19,11 @@ public class Juego extends JPanel{
         g.fillRect(500, 500, 40, 80);
     }
     
+class KeyChecker extends KeyAdapter{
+    public void keyPressed(KeyEvent event){
+        char ch = event.getKeyChar();
+        System.out.println(event.getKeyChar());
+    }
+}
 }
 
