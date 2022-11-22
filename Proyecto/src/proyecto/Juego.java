379e3f37dc8afda496acf45b4c86241ba1ad9f38
orbb4 <reiturra2021@udec.cl>
2022-11-22 17:20:49 -0300

    El angulo de las ruedas del auto ahora puede ser cambiado usando el metodo setAngulo de una instancia de Ruedas

--- Proyecto/src/proyecto/Juego.java
+++ Proyecto/src/proyecto/Juego.java
@@ -8,14 +8,16 @@ public class Juego extends JPanel{
         rect = new Rectangle(600, 200, 40, 80);
         this.setLayout(null); 
         ruedas = new Ruedas(rect);
+        ruedas.setAngulo(12, true, true, true, true);
     }
     public void paint(Graphics g){       
         super.paint(g);
         this.setBackground(Color.GREEN);
+        ruedas.paint(g);
         // prototipo. Se deberia llamar al paint de una instancia de Auto
         g.setColor(Color.RED);
         g.fillRect(rect.x, rect.y, rect.width, rect.height);
-        ruedas.paint(g);
+        
         
     }
     
