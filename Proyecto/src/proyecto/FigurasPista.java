fd216aa2f18d2b71f7fbbbf10f98a7ad5b83cdd2
cotecita <marcastillo2021@udec.cl>
2022-11-25 11:32:14 -0300

    se modifica la clase FigurasPista, añadiendo nuevas figuras a elección

--- Proyecto/src/proyecto/FigurasPista.java
+++ Proyecto/src/proyecto/FigurasPista.java
@@ -40,5 +40,79 @@ public class FigurasPista {
             g.setColor(Color.GRAY);
             g.fillRect(px-(ax-ly), py+(ax-ly), ax-ly, ly);
         }
+        
+        if(choice==5){
+            //2
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py, ly, ax);
+            g.setColor(Color.GRAY);
+            g.fillRect(px+ly, py+(ax-ly), ax-ly, ly);
+            
+            //union
+            g.setColor(Color.GRAY);
+            g.fillRect(px+ax, py+(ax-ly), ax, ly);
+            
+            //4
+            g.setColor(Color.GRAY);
+            g.fillRect(px+(2*ax)+ax-ly, py, ly, ax);
+            g.setColor(Color.GRAY);
+            g.fillRect(px+(2*ax), py+(ax-ly), ax-ly, ly);
+        }
+        
+        if(choice==6){
+            //1
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py, ax, ly);
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py+ly, ly, ax-ly);
+            
+            //union
+            g.setColor(Color.GRAY);
+            g.fillRect(px+ax, py, ax, ly);
+            
+            //3
+            g.setColor(Color.GRAY);
+            g.fillRect(px+2*ax, py, ax, ly);
+            g.setColor(Color.GRAY);
+            g.fillRect(px+2*ax+(ax-ly), py+ly, ly, ax-ly);
+        }
+        
+        if(choice==7){
+            //1
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py, ax, ly);
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py+ly, ly, ax-ly);
+            
+            //union
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py+ax, ly, ax);
+            
+            //3
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py+2*ax, ly, ax);
+            g.setColor(Color.GRAY);
+            g.fillRect(px+ly, py+2*ax+(ax-ly), ax-ly, ly);
+        }
+        
+        if(choice==8){
+            //3
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py, ax, ly);
+            g.setColor(Color.GRAY);
+            g.fillRect(px+(ax-ly), py+ly, ly, ax-ly);
+            
+            //union
+            g.setColor(Color.GRAY);
+            g.fillRect(px+(ax-ly), py+ax, ly, ax);
+            
+            //4
+            g.setColor(Color.GRAY);
+            g.fillRect(px+(ax-ly), py+2*ax, ly, ax);
+            g.setColor(Color.GRAY);
+            g.fillRect(px, py+2*ax+(ax-ly), ax-ly, ly);
+        }
+        
+        
     }
 }
--- Proyecto/src/proyecto/Pista.java
+++ Proyecto/src/proyecto/Pista.java
@@ -7,11 +7,20 @@ public class Pista{
     private FigurasPista p2;
     private FigurasPista p3;
     private FigurasPista p4;
+    private FigurasPista p5;
+    private FigurasPista p6;
+    private FigurasPista p7;
+    private FigurasPista p8;
     public Pista(){
-        p1 = new FigurasPista(300, 50, 150, 80);
-        p2 = new FigurasPista(600, 400, 150, 80);
-        p3 = new FigurasPista(300, 400, 150, 80);
-        p4 = new FigurasPista(600, 50, 150, 80);
+        p1 = new FigurasPista(0, 0, 75, 40);
+        p2 = new FigurasPista(100, 0, 75, 40);
+        p3 = new FigurasPista(200, 0, 75, 40);
+        p4 = new FigurasPista(350, 0, 75, 40);
+        p5 = new FigurasPista(0, 300, 75, 40);
+        p6 = new FigurasPista(400, 300, 75, 40);
+        p7 = new FigurasPista(800, 300, 75, 40);
+        p8 = new FigurasPista(1200, 300, 75, 40);
+
     }
      
      public void paint(Graphics g){
@@ -19,5 +28,9 @@ public class Pista{
          p2.paint(g, 2);
          p3.paint(g, 3);
          p4.paint(g, 4);
+         p5.paint(g, 5);
+         p6.paint(g, 6);
+         p7.paint(g, 7);
+         p8.paint(g, 8);
     }
 }
